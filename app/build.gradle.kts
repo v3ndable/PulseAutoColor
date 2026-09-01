a7bcc371ee90c300d4d887555d68858c8cc5fb35
v3ndable <armin.fluch08@gmail.com>
2026-09-01 23:34:04 +0200

    build: update gradle files for 1.1.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,13 @@ plugins {
 android {
     namespace = "com.vend.pulseautocolor"
     compileSdk = 37
-    buildToolsVersion = "37.0.0"
 
     defaultConfig {
         applicationId = "com.vend.pulseautocolor"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
@@ -49,16 +48,5 @@ android {
 dependencies {
     compileOnly(libs.libxposed.api)
     implementation(libs.libxposed.service)
-    implementation("androidx.palette:palette:1.0.0")
+    implementation(libs.androidx.palette)
 }
-
-/*
-dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    compileOnly("io.github.libxposed:api:102.0.0")
-    implementation("androidx.palette:palette:1.0.0")
-}*/
